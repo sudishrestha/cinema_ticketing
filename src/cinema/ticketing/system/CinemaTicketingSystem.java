bf03961f8c53b23959632e976dba78a9554a44da
Sudish Shrestha <sudishrestha@gmail.com>
2018-11-15 15:16:50 +0545

    code added

---
+++ src/cinema/ticketing/system/CinemaTicketingSystem.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cinema.ticketing.system;
+
+public class CinemaTicketingSystem {
+
+  public CinemaTicketingSystem()
+  {
+  
+  }
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Index ind = new Index();
+       ind.show();
+    }
+    
+}
